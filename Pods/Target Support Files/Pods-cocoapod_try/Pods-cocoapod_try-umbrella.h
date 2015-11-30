b8a4248d2e447061890ab76c740c52599bc3bc4b
有村琢磨 <arimuratakuma@aritaku.local>
2015-11-30 14:13:47 +0900

    new files

---
+++ Pods/Target Support Files/Pods-cocoapod_try/Pods-cocoapod_try-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_cocoapod_tryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_cocoapod_tryVersionString[];
+
--- cocoapod_try/ViewController.swift
+++ cocoapod_try/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import LTMorphingLabel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LTMorphingLabelDelegate {
+    
+    var textArray = ["おまってぃー", "ありたく", "くーちゃん", "しゅば"]
+    var i = 0
+    
+    var text:String {
+        get {
+            if i >= textArray.count {
+                i = 0
+            }
+            return textArray[i++]
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    @IBOutlet var label: LTMorphingLabel!
+    
+    func changeText(sender: AnyObject) {
+        label.text = text
+    }
+    
+    @IBAction func segmentChanged(sender: UISegmentedControl) {
+        let seg = sender
+        switch seg.selectedSegmentIndex {
+        case 0:
+            self.label.morphingEffect = .Anvil
+        case 1:
+            self.label.morphingEffect = .Burn
+        case 2:
+            self.label.morphingEffect = .Evaporate
+        case 3:
+            self.label.morphingEffect = .Scale
+        case 4:
+            self.label.morphingEffect = .Fall
+        default:
+            self.label.morphingEffect = .Pixelate
+        }
+        self.changeText(sender);
     }
 
     override func didReceiveMemoryWarning() {
